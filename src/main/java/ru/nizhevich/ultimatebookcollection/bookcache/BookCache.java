4f15353a68209f4b7319dafac49cfe83d818e1a6
kobe <ramonkb8@gmail.com>
2024-02-12 14:51:13 +0300

    com-4: Новый баг с path. Пришлось сделать явную загрузку ресурсов.

--- src/main/java/ru/nizhevich/ultimatebookcollection/bookcache/BookCache.java
+++ src/main/java/ru/nizhevich/ultimatebookcollection/bookcache/BookCache.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import ru.nizhevich.ultimatebookcollection.exception.EmptyCacheException;
 import ru.nizhevich.ultimatebookcollection.model.Book;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -28,13 +31,23 @@ import java.util.Objects;
 public class BookCache {
 
     private final String path;
+    private File csvFile;
+    private static List<Book> allBooksFromCsvFile;
 
     @Autowired
     public BookCache(@Value("${csv.path}") String path) {
         this.path = path;
+        loadResource();
     }
 
-    private static List<Book> allBooksFromCsvFile;
+    private void loadResource() {
+        Resource resource = new ClassPathResource(path);
+        try {
+            this.csvFile = resource.getFile();
+        } catch (IOException e) {
+            throw new EmptyCacheException("The CSV file path is invalid. Check the path to CSV file");
+        }
+    }
 
     public List<Book> getAllBooksFromCsvFile() throws EmptyCacheException {
         if(Objects.isNull(allBooksFromCsvFile) || allBooksFromCsvFile.isEmpty()) {
@@ -51,7 +64,7 @@ public class BookCache {
     private List<Book> initCsvFile() {
         CsvToBean<Book> csvToBean = null;
         List<Book> resultBookList = null;
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             csvToBean = new CsvToBeanBuilder<Book>(br)
                     .withType(Book.class)
                     .withIgnoreEmptyLine(true)
